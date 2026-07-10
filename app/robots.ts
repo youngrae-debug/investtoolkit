06755f51a61a62ee666dfbe7ae13ee36a6b63355
youngrae-debug <cho02116@gmail.com>
2026-07-10 13:35:57 +0900

    Improve trust, accessibility and calculation safety

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,1 @@
-import type {MetadataRoute} from 'next';export const dynamic='force-static';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://investtoolkit.kr/sitemap.xml'}}
+import type {MetadataRoute} from 'next';export const dynamic='force-static';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://invetk.com/sitemap.xml'}}
